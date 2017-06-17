1b21d0f19cdbf5fc2d1dac2110d93e0f27dd3be7
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2017-06-17 22:30:34 +0200

    Forgot a few comments

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ import sys
 from cx_Freeze import setup, Executable
 import os
 
+# Set missing path variables
 python_path = r"C:\Users\EUC\AppData\Local\Programs\Python\Python35"
-
 os.environ['TCL_LIBRARY'] = os.path.join(python_path, "tcl", "tcl8.6")
 os.environ['TK_LIBRARY'] = os.path.join(python_path, "tcl", "tk8.6")
 
 package_list = ["watermark", "tkinter", "os"]
 
+# Required files in include folder:
+# tk86t.dll
+# tcl86t.dll
+# From the tcl subfolder of the python folder
 build_exe_options = {"packages": package_list,
                      "include_files": [os.path.join("include", file) for file
                                        in os.listdir("include")]}

1bab549f35fbfa5b78241a7bf980951e12197e4f
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2017-07-13 01:22:23 +0200

    Add a progress counter

--- watermark/UI/worker.py
+++ watermark/UI/worker.py
@@ -10,7 +10,7 @@ from watermark.tools.watermarker import WaterMarker
 
 class Worker(Frame):
     """
-    Worker gui elements, does all the actual work to the images with the 
+    Worker gui elements, does all the actual work to the images with the
     watermarker class and contains progressbar and startbutton
     """
     def __init__(self, file_selector, options_pane, master=None):
@@ -25,6 +25,7 @@ class Worker(Frame):
         self.watermarker = WaterMarker()
 
         self.progress_var = IntVar()
+        self.file_count = IntVar()
         self.progress_bar = Progressbar(self, orient="horizontal",
                                         mode="determinate", length=600,
                                         variable=self.progress_var)
@@ -34,11 +35,17 @@ class Worker(Frame):
                                    command=self.apply_watermarks)
         self.stop_button = Button(self.button_frame, text="Stop",
                                   command=self.stop_work)
+        self.counter_frame = Frame(self)
 
         self.create_widgets()
 
     def create_widgets(self):
         """Create GUI"""
+        Label(self.counter_frame, textvariable=self.progress_var).pack(side=LEFT)
+        Label(self.counter_frame, text="/").pack(side=LEFT)
+        Label(self.counter_frame, textvariable=self.file_count).pack(side=LEFT)
+        self.counter_frame.pack()
+
         self.progress_bar.pack()
 
         self.stop_button.config(state=DISABLED)
@@ -52,6 +59,7 @@ class Worker(Frame):
         and prepare te progress bar
         """
         files = self.file_selector.get_file_paths()
+        self.file_count.set(len(files))
         self.progress_bar.configure(maximum=len(files))
         for file in files:
             self.image_que.put(file)
@@ -76,7 +84,7 @@ class Worker(Frame):
 
     def start_work(self):
         """
-        The baby factory, spawns child workers to apply the watermark to 
+        The baby factory, spawns child workers to apply the watermark to
         the images
         """
         try:
@@ -97,6 +105,7 @@ class Worker(Frame):
     def reset(self):
         self.image_que = queue.Queue()
         self.progress_var.set(0)
+        self.file_count.set(0)
         self.start_button.config(state=NORMAL)
         self.stop_button.config(state=DISABLED)
 
@@ -107,7 +116,7 @@ class Worker(Frame):
     def work(self, outpath, **kwargs):
         """
         Work instructions for the child workers
-        keep grabbing a new image path and then apply watermark with 
+        keep grabbing a new image path and then apply watermark with
         the watermarker, using option pane to create paths
         """
         while self.running:
@@ -116,6 +125,8 @@ class Worker(Frame):
             except queue.Empty:
                 self.start_button.config(state=NORMAL)
                 self.stop_button.config(state=DISABLED)
+                self.progress_var.set(0)
+                self.file_count.set(0)
                 self.running = False
                 return
             try:
@@ -128,7 +139,7 @@ class Worker(Frame):
                 return
             except Exception as e:
                 print("Error!\n", type(e), "\n", e)
-            self.progress_bar.step()
+            self.progress_bar.step(amount=1)
 
         self.reset()
 
